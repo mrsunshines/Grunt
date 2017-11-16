9226c5f3e9ecd855db52bfb2ac0035e7abc1d2e1
mrsunshines <k.hubertjankowski@gmail.com>
2017-11-16 23:42:50 +0100

    seems working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,68 +1,66 @@
 ﻿module.exports = function(grunt) {
-
   grunt.initConfig({
-    sass: {
-      options: {
-        sourceMap: true
-      },
-      dist: {
-        files: {
+      sass: {
+        options: {
+          sourceMap: true
+        },
+        dist: {
+          files: {
           'css/main.css': 'sass/main.sass'
+          }
         }
-      }
-    },
-    imagemin: {
-      dynamic: {
-        files: [{
+      },
+
+      imagemin: {
+        dynamic: {
+          files: [{
             expand: true,
             cwd: 'images/',
             src: ['**/*.{png,jpg,gif}'],
             dest: 'images/build/'
-        }]
-      }
-    },
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-      options: {
-        globals: {
-          jQuery: true
+          }]
         }
-      }
-    },
-    watch: {
-      scripts: {
-        files: ['sass/*.sass'],
-        tasks: ['sass'],
+      },
+
+      jshint: {
+        files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
         options: {
+          globals: {
+            jQuery: true
+          }
+        } 
+      },
+
+      watch: {
+        scripts: {
+          files: ['sass/*.sass'],
+          tasks: ['sass'],
+          options: {
             spawn: false,
+          }
         }
-    },
-    browserSync: {
-            dev: {
-                bsFiles: {
-                    src : [
-                        'css/*.css',
-                        '*.html'
-                    ]
-                },
-                options: {
-                    watchTask: true,
-                    server: './Grunt'
-                }
+      },
+
+      browserSync: {
+        default_options: {
+          bsFiles: {
+            src: ["css/*.css","*.html"]
+          },
+          options: {
+            watchTask: true,
+            server: {
+              baseDir: "./"
             }
+          }
         }
+      }  
+});
 
-     // files: ['<%= jshint.files %>'],
-     // tasks: ['jshint']
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-browser-sync');
-  
-  grunt.registerTask('default', ['jshint', 'sass', 'imagemin', 'browserSync', 'watch']);
-
+  grunt.registerTask('default', ['jshint','sass','imagemin','browserSync','watch']);
 };
+
